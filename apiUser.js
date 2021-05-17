79486413ccd8c7280452e4475ad605c3c0496bd3
fdionj <fdionj@gmail.com>
2021-05-17 21:08:03 +0700

    commit to 'master'

--- apiUser.js
+++ apiUser.js
@@ -31,7 +31,8 @@ const signUp = (request, response) => {
         response
           .status(200)
           .json({code: 201, message: 'Username or Email Already Registered!'});
-        return;
+          console.log('Masuk');
+          return;
       }
       response
         .status(200)
