9757529cd9890c830529a11111a97270db53038d
fdionj <fdionj@gmail.com>
2021-05-22 13:35:36 +0700

    Update apiGlobal.js

--- apiGlobal.js
+++ apiGlobal.js
@@ -14,14 +14,14 @@ const pool = new Pool({
 });
 
 const getAllCategory = (request, response) => {
-  return pool.query(
+  pool.query(
     'SELECT category_id AS id, category_name AS name, category_desc AS desc, category_image AS image FROM category ORDER BY category_id',
     (error, results) => {
       if (error) {
-        return response.status(200).json({ code: 201, message: 'Server Error!' });
+        response.status(200).json({ code: 201, message: 'Server Error!' });
         return;
       }
-      return response.status(200).json(results.rows);
+      response.status(200).json(results.rows);
     },
   );
 };
