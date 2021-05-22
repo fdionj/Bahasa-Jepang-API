11bc220ae9ce955ff6ff42d81aaa4adf7cf99a1d
fdionj <fdionj@gmail.com>
2021-05-22 18:08:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,6 @@ app.use(bodyParser.json());
 app.set('trust proxy', 1);
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-});
 app.use(
   bodyParser.urlencoded({
     extended: true,
